f7d359c48fefaff49ae91cfc06f062be58c96e3e
Nozdyakov <nozdryakovtema0603@gmail.com>
2023-05-20 02:06:43 +0300

    create-project

---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>my front-table project</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h1{
+    color: aqua;
+}
+</style>
